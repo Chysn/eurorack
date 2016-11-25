95c90107b0269d61def3c02de789e379d2531861
Jason Justian <jjustian@gmail.com>
2016-11-25 17:26:03 -0500

    Experimentally calibrate chromatic coefficients

--- peaks/chromatic_quantize.h
+++ peaks/chromatic_quantize.h
@@ -22,10 +22,19 @@ inline int16_t chromatic_quantize(int16_t raw) {
 	if (ix < 0) ix = -ix;
 	if (ix > 12) ix = 12; // Range-checking
 
-	/* (((12th root of 2) ^ n) - 1) * 100000, where n = note, 0 = tonic */
+	/* (((12th root of 2) ** n) - 1) * 100000, where n = note, 0 = tonic */
+    /* These are arithmetically-determined coefficients based on
+           (((12th root of 2) ** n) - 1) * 100000, where n = note, 0 = tonic
+       But they aren't very accurate so see below
 	uint32_t coeff [] = {      0,   5946,  12246,  18921,
 						   25992,  33484,  41421,  48930,
 						   58740,  68179,  78180,  88775,
+						  100000 };
+    */
+	/* These are experimentally-calibrated coefficients: */
+	uint32_t coeff [] = {      0,   7300,  16000,  25592,
+						   33484,  41421,  49200,  57500,
+						   67000,  75500,  84000,  91500,
 						  100000 };
 	note = (coeff[ix] * BEIGEMAZE_CHROMATIC_VOLT) / coeff[12];
 	if (raw < 0) note = -note;
